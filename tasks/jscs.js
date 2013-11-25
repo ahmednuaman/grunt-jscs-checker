3f59b7f004f10e5a6538f528da377fc89e7eb9ee
Ahmed Nuaman <ahmed.nuaman@bskyb.com>
2013-11-25 13:27:33 +0000

    added junit XML reporter

--- tasks/jscs.js
+++ tasks/jscs.js
@@ -3,6 +3,7 @@ module.exports = function( grunt ) {
 
     var fs = require( "fs" );
     var path = require( "path" );
+    var builder = require( "xmlbuilder" );
     var Checker = require( "jscs/lib/checker" );
     var defaults = {
         config: ".jscs.json"
@@ -15,6 +16,7 @@ module.exports = function( grunt ) {
         var cfgPath = options.config;
         var files = this.filesSrc;
         var done = this.async();
+        var junitXML = options.junit ? builder.create("testsuites") : null;
 
         if ( !grunt.file.isPathAbsolute( cfgPath ) ) {
             // Prepend the cwd, as jscs does via CLI
@@ -32,11 +34,25 @@ module.exports = function( grunt ) {
 
         errorCount = i = 0;
 
+        if (junitXML) {
+            junitXML.ele("testsuite", {
+                name: "JSCS",
+                timestamp: (new Date()).toISOString().substr(0, 19)
+            });
+        }
+
         function update() {
             i++;
 
             // Does all promises have been run?
             if ( i === files.length ) {
+                if (junitXML) {
+                    junitXML.att("tests", files.length);
+                    junitXML.att("errors", errorCount);
+
+                    grunt.file.write(options.junit, junitXML.end());
+                }
+
                 if ( errorCount > 0 ) {
                     grunt.log.error( errorCount + " code style errors found!" );
                     done( false );
@@ -55,10 +71,20 @@ module.exports = function( grunt ) {
             }
 
             promise.then(function( errors ) {
+                if (junitXML) {
+                    var spec = junitXML.ele("testcase", {
+                      name: errors.getFilename()
+                    });
+                }
+
                 if ( !errors.isEmpty() ) {
                     errors.getErrorList().forEach(function( error ) {
                         errorCount++;
                         grunt.log.writeln( errors.explainError( error, true ) );
+
+                        if (junitXML) {
+                            spec.ele("failure", {}, errors.explainError( error ));
+                        }
                     });
                 }
 
